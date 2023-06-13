a01f790d50add2c44e23369c2383482b13517d40
Lucas Del Puerto Garcia <lucas.d.puerto@outlook.com>
2023-06-13 12:51:47 -0300

    :fire: remove unused files

--- functions/src/database/connection.ts
+++
@@ -1,9 +0,0 @@
-import {Pool} from "pg";
-
-export const connectionPool = new Pool({
-  host: "rds-postgres-choco-tech.cq72brsxchlx.us-east-2.rds.amazonaws.com",
-  port: 5432,
-  user: "choco_admin",
-  database: "choco_iot",
-  password: "ChocoGilbertoIOT",
-});
